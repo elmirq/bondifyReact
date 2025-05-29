e18f755587ee8c1d7adaa62b0df79d89d8b193c2
ElmirQ <elmir.qasimzade@gmail.com>
2025-05-29 11:06:59 +0400

    Footer and popup

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -20,15 +20,15 @@ const PopUp = () => {
   return (
     <div className="card position-fixed bottom-0 start-0 mb-1 ms-1" id="popUp">
       <h5 className="card-header">
-        Feaed
+        Lorem, ipsum.
         <button type="button" id="baglamaButton" className="btn btn-sm position-absolute top-0 end-0 m-2">
           <i className="bi bi-x-lg"></i>
         </button>
       </h5>
       <div className="card-body">
-        <h5 className="card-title">baljblaj</h5>
-        <p className="card-text">Bla bla bla</p>
-        <a href="#" className="btn btn-secondary position-absolute bottom-0 end-0 me-4 mb-4">Elaqe</a>
+        <h5 className="card-title">Lorem ipsum dolor sit amet.</h5>
+        <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur iusto ipsum vero dolorum porro nesciunt non rem laborum enim reprehenderit!</p>
+        <a href="#" className="btn btn-secondary position-absolute bottom-0 end-0 me-4 mb-4">Əlaqə</a>
       </div>
     </div>
   );
